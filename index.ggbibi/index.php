c8a2df828481825f7cead277b72cb0ad0c16d079
bibi-santos <gabrielasantos211200@gmail.com>
2026-09-17 09:28:42 -0300

    Add files via upload

---
+++ index.ggbibi/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome= "";
+$datanas= "";
+$cpf= "";
+$rg= "";
+$endereco= "";
+$cidade= "";
+$anime= "";
+$filme= "";
+$serie= "";
+
+echo $nome;
+echo "Meu nome é:" .$nome. "Nasci em:" .$datanas. "Meu cpf é:" .$cpf. 
+    "Meu rg é:" .$rg. "Moro no endereço:" .$endereco. "A cidade que moro é:" .$cidade.
+    "O anime que eu mais gosto é:" .$anime. "O filme que eu mais gosto é:" .$filme. 
+    "A minha série favorita é:" .$serie;
+?>
